1ac477837f2e59ae7beed33b71e3e7602a5a4087
Camille LOrtye <214392280+fortune1220@users.noreply.github.com>
2026-05-18 15:19:11 +0000

    Producten uit database ophalen

--- KE03_INTDEV_SE_1_Base-main/KE03_INTDEV_SE_1_Base/Pages/Index.cshtml.cs
+++ KE03_INTDEV_SE_1_Base-main/KE03_INTDEV_SE_1_Base/Pages/Index.cshtml.cs
@@ -9,20 +9,26 @@ namespace KE03_INTDEV_SE_1_Base.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ICustomerRepository _customerRepository;
+        private readonly IProductRepository _productRepository;
 
         public IList<Customer> Customers { get; set; }
+        public IList<Product> Products { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger, ICustomerRepository customerRepository)
+        public IndexModel(ILogger<IndexModel> logger, ICustomerRepository customerRepository, IProductRepository productRepository)
         {
             _logger = logger;
             _customerRepository = customerRepository;
             Customers = new List<Customer>();
+            _productRepository = productRepository;
+            Products = new List<Product>();
         }
 
         public void OnGet()
         {            
-            Customers = _customerRepository.GetAllCustomers().ToList();                            
+            Customers = _customerRepository.GetAllCustomers().ToList();   
+            Products = _productRepository.GetAllProducts().ToList();
             _logger.LogInformation($"getting all {Customers.Count} customers");
         }
+        
     }
 }
