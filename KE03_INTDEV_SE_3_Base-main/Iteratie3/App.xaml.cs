50ecbbe3b89934f0c716378b2391d678e0ba5a8b
tnoa <noascipio1@gmail.com>
2026-06-09 10:16:55 +0200

    iteratie 3 klaar gezet
    iteratie 3 klaar gezet

---
+++ KE03_INTDEV_SE_3_Base-main/Iteratie3/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Iteratie3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ KE03_INTDEV_SE_3_Base-main/Iteratie3/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Iteratie3")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Iteratie3.Pages")]
