6a3fe823f0d92d56fe04c67e9ea4dd18d3df557f
tnoa <noascipio1@gmail.com>
2026-06-11 09:52:00 +0200

    Redoing project with sample content
    Redoing project with sample content

--- KE03_INTDEV_SE_3_Base-main/Iteratie3/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Iteratie3")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Iteratie3.Pages")]
